2b325039360012312c8968b54942cb4558149569
cheung <982450544@qq.com>
2018-12-20 23:21:59 +0800

    test connect mysql

---
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Model\AdminUser;
+
+class AdminUserController extends Controller
+{
+    public function index(Request $request) {
+    	return AdminUser::all();
+    }
+}
