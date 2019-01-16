a2f5b31be1b5d3b39decd80bad2609eb444f5cb0
zt <982450544@qq.com>
2019-01-16 13:44:36 +0800

    跨域

--- app/Http/Middleware/CrossRequestMiddleware.php
+++ app/Http/Middleware/CrossRequestMiddleware.php
@@ -16,12 +16,12 @@ class CrossRequestMiddleware
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
+//        $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
 //        $allow_origin = [
 //            'http://localhost:8000',
 //        ];
 //        if (in_array($origin, $allow_origin)) {
-            $response->header('Access-Control-Allow-Origin', $origin);
+            $response->header('Access-Control-Allow-Origin', '*');
             $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
             $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
             $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
